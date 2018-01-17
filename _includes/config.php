51051cbbee9e317821269079bc41d431774a59ae
GGBOT <th.boerma@gmail.com>
2018-01-17 21:02:20 +0100

    Gewoon techen

--- _includes/config.php
+++ _includes/config.php
@@ -1,17 +1,6 @@
 <?php
 
-// database connection parameters
-$HOST  =    '127.0.0.1'; // if hosting on localhost, use 127.0.0.1 instead of localhost : this is SECONDS faster in performance when handling big data.
-$USER  =    'root';
-$PASS  =    '';
-$DB    =    'chargen';
-
-// create the mysqli connection.
-$UPLINK = mysqli_connect($mHOST, $mUSER, $mPASS, $mDB);
-  unset($HOST);
-  unset($USER);
-  unset($PASS);
-  unset($DB);
+include_once('db.php');
 
 // config variable.
 $APP = array();

c638576263d043b2f6f049c13406c61a70837d4a
goblinbot <th.boerma@gmail.com>
2018-01-29 09:53:35 +0100

    [0005] JS and overflows
    Moved inline JS to functions.js, added styling for overflowing in the
    main 'cell'.

--- _includes/js/functions.js
+++ _includes/js/functions.js
@@ -7,7 +7,6 @@ function checkGridSupport() {
 		}
 		catch(err) {
 			/* browser cannot handle the CSS.supports command => assume this browser is ancient eldritch wizardry at this point. */
-
 		}
 
 	if(result == false) {
@@ -16,6 +15,53 @@ function checkGridSupport() {
 
 	} else {
 		/* Grid support detected! This is a nice browser.*/
+	}
+}
+
+$(document).ready(function(){
+
+	var maincell = $('.main');
+
+	if(maincell && $(window).width() > 768) {
+
+		function scrollMain() {
+
+			var offsetTop = maincell.scrollTop();
+			var offsetBottom = maincell.scrollTop() + maincell.height();
+
+			if(offsetTop > 2) {
+				maincell.addClass('offsetTop');
+			} else {
+				maincell.removeClass('offsetTop');
+			}
+
+		}
+
+		/* adds the scroll function to the MAIN div. */
+		maincell.scroll(function() {
+		    scrollMain();
+		});
+
+		/* run the function at start incase of refresh. */
+		scrollMain();
 
 	}
+
+	/* init */
+	setTimeout(function(){
+		$('body').removeClass('notransition');
+	},400);
+
+	checkGridSupport();
+});
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("myP").className = "test";
+    } else {
+        document.getElementById("myP").className = "";
+    }
 }
--- footer.php
+++ footer.php
@@ -8,13 +8,6 @@
 </div> <!-- grid -->
   <?=EMSincludeJS()?>
   <script type="text/javascript">
-    $(document).ready(function(){
-      setTimeout(function(){
-        $('body').removeClass('notransition');
-      },400);
-
-      checkGridSupport();
-    });
   </script>
 </body>
 </html>

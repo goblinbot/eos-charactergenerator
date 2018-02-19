a7a66fa5fdb29bcf4c2a27205846aa6e8383e57f
GGBOT <th.boerma@gmail.com>
2018-02-20 00:44:10 +0100

    fixed the sample
    Oh man..

--- db.sample.php
+++ db.sample.php
@@ -1,12 +1,10 @@
 <?php
-if (!isset($APP)) die('No direct access allowed');
 
 // database connection parameters
 $HOST  =    '127.0.0.1'; // if hosting on localhost, use 127.0.0.1 instead of localhost : this is SECONDS faster in performance when handling big data.
 $USER  =    'username';
 $PASS  =    'passcode';
 $DB    =    'my_database';
-
 
 // create the mysqli connection.
 $UPLINK = mysqli_connect($HOST, $USER, $PASS, $DB);

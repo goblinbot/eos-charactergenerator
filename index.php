41a445fa17b195787d4b90f1fb5692151f1994b9
GGBOT <th.boerma@gmail.com>
2018-03-22 23:18:04 +0100

    [Optimize] Character basic info form

--- index.php
+++ index.php
@@ -176,38 +176,38 @@
                   "<div class=\"formitem\">"
                     ."<h3><i class=\"fas fa-user\"></i>&nbsp;Character Name</h3>"
                     ."<input type=\"text\" placeholder=\"Character Name\" maxlength=\"99\" name=\"editchar[character_name]\" value=\"".EMS_echo($character['character_name'])."\"></input>"
-                  ."</div>"
-                  ."<br/>";
+                  ."</div>";
+                  // ."<br/>";
 
                 $printresult .=
                   "<div class=\"formitem\">"
                     ."<h3><i class=\"fas fa-users\"></i>&nbsp;Faction</h3>"
                     ."<p class=\"text-muted\">".ucfirst(EMS_echo($character['faction']))."</p>"
                   ."</div>"
-                  ."<br/>"
+                  // ."<br/>"
 
                   ."<div class=\"formitem\">"
-                    ."<h3><i class=\"fas fa-users\"></i>&nbsp;ICC Number:</h3>"
+                    ."<h3><i class=\"fas fa-key\"></i>&nbsp;ICC Number:</h3>"
                     ."<p class=\"text-muted\">".EMS_echo($character['ICC_number'])."</p>"
                   ."</div>"
-                  ."<br/>"
+                  // ."<br/>"
 
                   ."<div class=\"formitem\">"
                     ."<h3><i class=\"far fa-calendar-alt\"></i>&nbsp;Birth date</h3>"
                     ."<input type=\"text\" placeholder=\"..( current IC year: 240NT )\" maxlength=\"24\" name=\"editchar[ic_birthday]\" value=\"".EMS_echo($character['ic_birthday'])."\"></input>"
-                  ."</div>"
-                  ."<br/>";
+                  ."</div>";
+                  // ."<br/>";
 
                 $printresult .=
                   "<div class=\"formitem\">"
                     ."<h3><i class=\"fas fa-globe\"></i>&nbsp;Birth planet</h3>"
                     ."<input type=\"text\" placeholder=\"...\" maxlength=\"99\" name=\"editchar[birthplanet]\" value=\"".EMS_echo($character['birthplanet'])."\"></input>"
-                  ."</div>"."<br/>"
+                  ."</div>"/*."<br/>"*/
                   ."<div class=\"formitem\">"
                     ."<h3><i class=\"fas fa-globe\"></i>&nbsp;Current/home planet</h3>"
                     ."<input type=\"text\" placeholder=\"...\" maxlength=\"99\" name=\"editchar[homeplanet]\" value=\"".EMS_echo($character['homeplanet'])."\"></input>"
-                  ."</div>"
-                  ."<br/>";
+                  ."</div>";
+                  /*."<br/>"*/
 
                 $printresult .=
                   "<div class=\"formitem\">"

6f7fb2f481ead9e8a8c38f2bdbbc6e549b75c2ad
GGBOT <th.boerma@gmail.com>
2019-08-03 21:39:13 +0200

    Update convert_skills.php

--- dev/convert_skills.php
+++ dev/convert_skills.php
@@ -12,7 +12,7 @@ echo "<style>th,td { padding: 0 5px; }</style>";
 $sql = "SELECT count(id) FROM ecc_char_skills";
 $res = mysqli_fetch_assoc($UPLINK->query($sql));
 
-(int)$LIMIT = 200;
+(int)$LIMIT = 133;
 (int)$COUNT = $res['count(id)'];
 $RATIO = $COUNT / $LIMIT;
 $ROUND = ceil($RATIO);
@@ -22,7 +22,11 @@ echo "Limit: $LIMIT <br/>";
 echo "Ratio: $RATIO<br/>";
 echo "Round: $ROUND<br/>";
 
-$NEXT = ((int)$_GET['limit'] + 1);
+if (!isset($_GET['limit']) || $_GET['limit'] == '') {
+  $NEXT = 1;
+} else {
+  $NEXT = ((int)$_GET['limit'] + 1);
+}
 echo "Next: $NEXT<br/>";
 
 if(isset($_GET['limit']) && (int)$_GET['limit'] < 0) {
@@ -64,16 +68,17 @@ $res = $UPLINK->query($sql);
 if($res) {
   if(mysqli_num_rows($res) > 0) {
 
-    // echo "<table>
-    // <tr>
-    // <th style=\"width: 50px;\">#ID</th>
-    //   <th style=\"width: 50px;\">SheetID</th>
-    //   <th style=\"width: 550px;\">Character ID</th>
-    // </tr>";
+    echo "<table>
+    <tr>
+    <th style=\"width: 50px;\">#ID</th>
+      <th style=\"width: 50px;\">SheetID</th>
+      <th style=\"width: 550px;\">Character ID</th>
+      <th>.</th>
+    </tr>";
 
     while($row = mysqli_fetch_assoc($res)){
 
-      if($row['characterID'] == NULL || $row['characterID'] == "") {
+      // if($row['characterID'] == NULL || $row['characterID'] == "") {
 
 
         // echo "<tr>
@@ -86,13 +91,13 @@ if($res) {
         // $sql3 = "DELETE FROM `ecc_char_skills` WHERE id = '".$row['id']."' ";
         // $res3 = $UPLINK->query($sql3);
 
-      } else {
+      // } else {
 
-        // echo "<tr>
-        //   <td style=\"width: 50px;\">#{$row['id']}</td>
-        //   <td style=\"width: 50px;\">{$row['char_sheet_id']}</td>
-        //   <td style=\"width: 550px;\"> => {$row['characterID']}</td>
-        // </tr>";
+        echo "<tr>
+          <td style=\"width: 50px;\">#{$row['id']}</td>
+          <td style=\"width: 50px;\">{$row['char_sheet_id']}</td>
+          <td style=\"width: 550px;\"> => {$row['characterID']}</td>
+        ";
 
         $sql2 = "UPDATE `ecc_char_skills`
           SET `char_sheet_id` = '{$row['characterID']}'
@@ -100,12 +105,16 @@ if($res) {
           LIMIT 1";
         $update = $UPLINK->query($sql2) or trigger_error(mysqli_error($UPLINK));
 
-      }
 
-      // echo "<tr><td colspan=\"5\">$i</td></tr>";
+        echo "<td>-</td>
+        </tr>";
+
+      // }
+
+      echo "<tr><td colspan=\"5\">$i</td></tr>";
     }
 
-    // echo "</table>";
+    echo "</table>";
 
     echo '<script type="text/javascript">window.location = "convert_skills.php?limit='.$NEXT.'"</script>';
 

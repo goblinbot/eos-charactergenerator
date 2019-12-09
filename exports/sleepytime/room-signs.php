2385a4a58a238019380cb80c7059401a6fe4d8aa
Josh Goldberg <joshgoldberg@quickenloans.com>
2019-12-09 13:43:23 -0500

    Update room signs for figu/sl

--- exports/sleepytime/room-signs.php
+++ exports/sleepytime/room-signs.php
@@ -56,18 +56,18 @@ while($room_row = mysqli_fetch_assoc($room_res)){
     left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
     left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
     left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 58)
-    where CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,'')) = '$room' AND v2.field_value = '$building' AND v5.field_value = 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR
+    where CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,'')) = '$room' AND v2.field_value = '$building' 
+    AND v5.field_value = 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR
     (r.published in (0,1) AND r.payment_method = 'os_offline'))AND v2.field_value NOT LIKE 'medische%'
     UNION
-    select r.id, v6.field_value as foodlocation, CONCAT(v5.field_value,' ',r.first_name, ' ', SUBSTRING(r.last_name,1,1),'.') as name, v2.field_value as building, CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,'')) as room from joomla.jml_eb_registrants r
-    left join joomla.jml_eb_field_values v1 on (v1.registrant_id = r.id and v1.field_id = 21)
-    left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 36)
-    left join jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
-    left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
-    left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
-    left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 58)
-    where CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,'')) = '$room' AND v2.field_value = '$building' AND v5.field_value != 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR
-    (r.published in (0,1) AND r.payment_method = 'os_offline'))AND v2.field_value NOT LIKE 'medische%' 
+    select r.id, v6.field_value as foodlocation, CONCAT(v5.field_value,' ',r.first_name, ' ', COALESCE(v6.field_value,''),' ', SUBSTRING(r.last_name,1,1),'.') as name, 'tweede gebouw' as building, 
+    NULL as bastion_room, CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,'')) as tweede_room from joomla.jml_eb_registrants r
+    left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 73)
+    left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 72)
+    left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
+    left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 16)
+    where v5.field_value != 'Speler' AND 'tweede gebouw' =  '$building' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR 
+    (r.published in (0,1) AND r.payment_method = 'os_offline'))
     UNION 
     select r.id, v7.field_value as foodlocation, SUBSTRING_INDEX(SUBSTRING_INDEX(v1.field_value,' - ',1),' - ',-1) as name,
     LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) as building, substring_index(LEFT(v6.field_value,LOCATE(' - ',v6.field_value) - 1),',',-1) as room 

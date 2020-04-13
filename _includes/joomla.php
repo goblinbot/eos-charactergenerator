19b7bfdf53b933b38c25d85f4ad253402f042b43
Ubuntu <ubuntu@ip-172-31-36-97.eu-central-1.compute.internal>
2020-04-13 21:45:57 +0000

    Fix Figurant/SL/Keuken Crew display

--- _includes/joomla.php
+++ _includes/joomla.php
@@ -5,7 +5,7 @@ define( '_JEXEC', 1 );
 define( 'DS', '/' );
 // IMPORTANT: adjust path based on folder or define it manually as string
 // "myjoomlaroot" is name of your Joomla root folder
-define( 'JPATH_BASE', '/opt/bitnami/apache2/htdocs/');
+define( 'JPATH_BASE', '/var/www/html/');
 require_once ( JPATH_BASE .DS. 'includes' .DS. 'defines.php' );
 require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
 $mainframe = JFactory::getApplication('site');

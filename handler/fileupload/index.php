8bc2af640c561f03a51dc79fb21d041fbb6d71a2
Josh Goldberg <joshgoldberg@quickenloans.com>
2020-04-27 17:32:48 -0400

    Update index.php

--- handler/fileupload/index.php
+++ handler/fileupload/index.php
@@ -1,8 +1,16 @@
 <?php
+  // globals
+  include_once($_SERVER["DOCUMENT_ROOT"] . "/eoschargen/_includes/config.php");
+  include_once($APP["root"] . "/_includes/functions.global.php");
+  include_once($APP["root"] . "/header.php");
+
+  if(!isset($_SESSION)) {
+    session_start();
+  }
 ?>
 <html>  
     <head>  
-		
+		<script src="./_includes/css/style.css"
 		<script src="jquery.min.js"></script>  
 		<script src="bootstrap.min.js"></script>
 		<script src="croppie.js"></script>

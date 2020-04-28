191b5719cd132fd63c36477006df43429f29d7ed
Ubuntu <ubuntu@ip-172-31-36-97.eu-central-1.compute.internal>
2020-04-28 14:19:39 +0000

    update jquery used by fileupload

--- handler/fileupload/index.php
+++ handler/fileupload/index.php
@@ -45,8 +45,9 @@ if (!isset($APP)) die('No direct access allowed');
 
   <link rel="stylesheet" type="text/css" href="<?=$APP['header']?>/_includes/css/reset.css" />
   <link rel="stylesheet" type="text/css" href="<?=$APP['header']?>/_includes/css/style.css" />
-		<script src="jquery.min.js"></script>  
-		<script src="bootstrap.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 		<script src="croppie.js"></script>
 		<link rel="stylesheet" href="croppie.css" />
     </head>  

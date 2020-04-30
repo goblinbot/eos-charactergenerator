c84d49dda4d1af97cdc41119eedd0e031a3c0c51
Josh G <goldjd@gmail.com>
2020-04-30 13:45:59 -0400

    Explanation Text and Security for Photo Upload (#21)
    ## Added
     * Explanation text and example photo for file upload screen
    ## Fixed
     * File upload screen validates that a valid character has been provided in &viewChar=*
    *  File upload screen validates that character ID provided in &viewChar belongs to $jid

--- handler/fileupload/index.php
+++ handler/fileupload/index.php
@@ -3,6 +3,26 @@
   include_once($_SERVER["DOCUMENT_ROOT"] . "/eoschargen/_includes/config.php");
   include_once($APP["root"] . "/_includes/functions.global.php");
   include_once($APP["root"] . "/header.php");
+
+  if(!isset($_SESSION)) {
+    session_start();
+  }
+// Validate that viewChar belongs to logged in Joomla User
+    $charID = $_GET["viewChar"];
+      $sql2 = "SELECT `characterID`,`accountID` FROM `ecc_characters` WHERE `characterID` = $charID";
+      $res2 = $UPLINK->query($sql2) or trigger_error(mysqli_error($UPLINK));
+      if (mysqli_num_rows($res2) > 0) {
+        $character = mysqli_fetch_assoc($res2);
+        if ($character['accountID'] != $jid) {
+ 	        header("location: {$APP['header']}/index.php");
+          exit();
+        }
+      }
+      else {
+      header("location: {$APP['header']}/index.php");
+      exit();
+      }
+//End Validation
 ?>
 
 <!DOCTYPE html>
@@ -23,10 +43,16 @@
 
   <div class="main cell" align="center">
       <div class="panel-body" align="center">
-      <h2>Select Profile Image</h2>
+      <h1>Select Profile Image</h2>
         <input type="file" name="upload_image" id="upload_image" />
         <br />
         <div id="uploaded_image"></div>
+        <div class="panel-body">
+        <h3>Please submit a photo of your head and shoulders, taken against a solid color (preferably white) background. <br />
+        Please use the below photos as an example of what you should submit. <br />
+	This will be printed on your character's IC ID Card, so ideally the photo should be of you dressed as your character would appear.</h3>
+        <img src="<?php echo $APP["header"]; ?>/img/Example.png">
+        </div>
       </div>
     </div>
 <?php include './footer.php'; ?>

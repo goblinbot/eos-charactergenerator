0baef98d8ff654455c59ae82c89c249ce46b45dc
Josh G <goldjd@gmail.com>
2021-09-27 22:11:37 +0200

    Did I add the Bastion sleepers?

--- exports/sleepybubble/room-signs.php
+++ exports/sleepybubble/room-signs.php
@@ -38,6 +38,16 @@
     left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 73)
     where 'tweede gebouw' = '$building' AND v5.field_value != 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR
     (r.published in (0,1) AND r.payment_method = 'os_offline'))
+    UNION
+    select CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,''),coalesce(v6.field_value,''),coalesce(v7.field_value,'')) as room from joomla.jml_eb_registrants r
+    left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 36)
+    left join jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
+    left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
+    left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
+    left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 72)
+    left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 73)
+    where 'Bastion' = '$building' AND v5.field_value != 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR
+    (r.published in (0,1) AND r.payment_method = 'os_offline'))
     UNION 
     select substring_index(LEFT(v6.field_value,LOCATE(' - ',v6.field_value) - 1),',',-1) as room from joomla.jml_eb_registrants r
     join joomla.jml_eb_field_values v1 on (v1.registrant_id = r.id and v1.field_id = 21)
@@ -73,6 +83,17 @@
     where v5.field_value != 'Speler' AND 'tweede gebouw' =  '$building' AND r.event_id = $EVENTID and (v3.field_value = '$room' or v4.field_value = '$room') AND
     ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR  (r.published in (0,1) AND r.payment_method = 'os_offline'))
     UNION 
+    SELECT r.id, v6.field_value as foodlocation, CONCAT(v5.field_value,' ',r.first_name, ' ', COALESCE(v2.field_value,''),' ', SUBSTRING(r.last_name,1,1),'.') as name, 
+    'tweede gebouw' as building, CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,'')) as room from joomla.jml_eb_registrants r
+    left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 16)
+    left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 73)
+    left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 72)
+    left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
+    left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 58)
+    where v5.field_value != 'Speler' AND 'bastion' =  '$building' AND r.event_id = $EVENTID and (v3.field_value = '$room' or v4.field_value = '$room') AND
+    ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR  (r.published in (0,1) AND r.payment_method = 'os_offline'))
+    UNION 
+
     select r.id, v7.field_value as foodlocation, SUBSTRING_INDEX(SUBSTRING_INDEX(v1.field_value,' - ',1),' - ',-1) as name,
     LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) as building, substring_index(LEFT(v6.field_value,LOCATE(' - ',v6.field_value) - 1),',',-1) as room 
     from joomla.jml_eb_registrants r

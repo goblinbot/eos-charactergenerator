9751442c9e8d6bc7e0cda227ba6e3483742f17b5
WafflesMcDuff <goldjd@gmail.com>
2021-11-01 08:45:47 +0100

    Fix sleepytime

--- exports/sleepytime/index.php
+++ exports/sleepytime/index.php
@@ -12,13 +12,14 @@ ini_set('display_errors', 1);
 // First TSQL Statement is for Spelers information
 $stmt = db::$conn->prepare("select r.id, v1.field_value as name, v2.field_value as building, v3.field_value as bastion_room, v4.field_value as tweede_room from joomla.jml_eb_registrants r
 join joomla.jml_eb_field_values v1 on (v1.registrant_id = r.id and v1.field_id = 21)
-join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 93)
-join joomla.jml_eb_field_values v6 on (v2.registrant_id = r.id and v2.field_id = 94)
-join jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
+join joomla.jml_eb_field_values v2 ON (v2.registrant_id = r.id AND v2.field_id = 36)
+left join joomla.jml_eb_field_values v6 ON (v6.registrant_id = r.id AND v6.field_id = 93)
+left join joomla.jml_eb_field_values v7 ON (v7.registrant_id = r.id AND v7.field_id = 94)
+ join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
 left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
 left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
 where v5.field_value = 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR 
-(r.published in (0,1) AND r.payment_method = 'os_offline')) AND (v2.field_value NOT LIKE 'Yes%' AND v6.field_value NOT LIKE 'No%')
+(r.published in (0,1) AND r.payment_method = 'os_offline')) AND (v6.field_value = 'No' OR v6.field_value IS NULL) AND (v7.field_value = 'No' OR v7.field_value IS NULL)
 UNION
 /* 
 This TSQL Statement Grabs Figuranten (with real bed), SLs and Keuken Crew in the Bastion
--- exports/sleepytime/room-signs.php
+++ exports/sleepytime/room-signs.php
@@ -22,14 +22,14 @@
             $building = $bldg_row['field_value'];
             //This makes the list of rooms
             $room_sql = "select CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,'')) as room from joomla.jml_eb_registrants r
-    left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 93)
-    left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 94)
+    left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 36)
+    left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 93)
+    left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 94)
     left join jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
     left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
     left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
-    
     where v2.field_value ='$building' AND v5.field_value = 'Speler' AND r.event_id = $EVENTID and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal')) OR
-    (r.published in (0,1) AND r.payment_method = 'os_offline')) AND (v2.field_value NOT LIKE 'Yes%' AND v6.field_value NOT LIKE 'Yes%')
+    (r.published in (0,1) AND r.payment_method = 'os_offline')) AND (v6.field_value = 'No' OR v6.field_value IS NULL) AND (v7.field_value = 'No' OR v7.field_value IS NULL)
     UNION
     select CONCAT(coalesce(v3.field_value,''),coalesce(v4.field_value,''),coalesce(v6.field_value,''),coalesce(v7.field_value,'')) as room from joomla.jml_eb_registrants r
     left join joomla.jml_eb_field_values v2 on (v2.registrant_id = r.id and v2.field_id = 93)

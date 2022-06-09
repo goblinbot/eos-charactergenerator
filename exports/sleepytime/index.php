65b197aa094e50210e50beb7f445d7aade8faf2d
Josh G <goldjd@gmail.com>
2022-06-09 21:54:15 +0200

    Feature/f14 eating (#31)
    * Eating location reports fixed
    
    * Allow override of eating locations
    
    * add eating override for SLs and figus
    
    * Fix allergy report for sl/figu overrides
    
    * Fix bedden indelling
    
    * Medical sleeping working for non-spelers
    
    * Make sure medical sleepers are not duplicated

--- exports/sleepytime/index.php
+++ exports/sleepytime/index.php
@@ -25,32 +25,35 @@ UNION
 /* 
 This TSQL Statement Grabs Figuranten (with real bed), SLs and Keuken Crew in the Bastion
 */
-select r.id, CONCAT(v5.field_value,' ',r.first_name, ' ', COALESCE(v6.field_value,''),' ', SUBSTRING(r.last_name,1,1),'.') as name, 'Bastion' as building, 
-CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,''),COALESCE(v8.field_value,'')) as bastion_room, NULL as tweede_room from joomla.jml_eb_registrants r
+select r.id, CONCAT(v5.field_value,' ',r.first_name, ' ', COALESCE(v6.field_value,''),' ', SUBSTRING(r.last_name,1,1),'.') as name, 'tweede gebouw' as building, 
+NULL as bastion_room, coalesce(substring_index(medicsleep.field_value,',',-1),CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,''),COALESCE(v8.field_value,''))) as tweede_room from joomla.jml_eb_registrants r
 left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 73)
 left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 72)
 left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
 left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 16)
 left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 59)
 left join joomla.jml_eb_field_values v8 on (v8.registrant_id = r.id and v8.field_id = 38)
-where ASCII(UPPER(LEFT(CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,''),COALESCE(v8.field_value,'')),1))) BETWEEN 64 AND 90 
-AND r.event_id = $EVENTID and ((v5.field_value != 'Speler' AND v7.field_value != 'No') or (v5.field_value = 'Keuken Crew' or v5.field_value = 'Spelleider')) 
+left join joomla.jml_eb_field_values medicsleep on (medicsleep.registrant_id = r.id and medicsleep.field_id = 71)
+where coalesce(substring_index(medicsleep.field_value,',',1),'') != 'tweede gebouw' AND ASCII(UPPER(LEFT(CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,''),COALESCE(v8.field_value,'')),1))) NOT BETWEEN 64 AND 90 
+AND r.event_id = $EVENTID and ((v5.field_value = 'Figurant' AND v7.field_value = 'Yes') or (v5.field_value = 'Keuken Crew' or v5.field_value = 'Spelleider')) 
 and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR 
 (r.published in (0,1) AND r.payment_method = 'os_offline'))
 UNION
 /* 
 This TSQL Statement Grabs Figuranten (with real bed), SLs and Keuken Crew in the tweede gebouw
 */
-select r.id, CONCAT(v5.field_value,' ',r.first_name, ' ', COALESCE(v6.field_value,''),' ', SUBSTRING(r.last_name,1,1),'.') as name, 'tweede gebouw' as building, 
-NULL as bastion_room, CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,''),COALESCE(v8.field_value,'')) as tweede_room from joomla.jml_eb_registrants r
+select r.id, CONCAT(v5.field_value,' ',r.first_name, ' ', COALESCE(v6.field_value,''),' ', SUBSTRING(r.last_name,1,1),'.') as NAME, 
+IFNULL(coalesce(substring_index(medicsleep.field_value,',',1)),'tweede gebouw') as building, NULL as bastion_room,
+coalesce(substring_index(medicsleep.field_value,',',-1), CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,''),COALESCE(v8.field_value,''))) as tweede_room from joomla.jml_eb_registrants r
 left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 73)
 left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 72)
 left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
 left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 16)
 left join joomla.jml_eb_field_values v7 on (v7.registrant_id = r.id and v7.field_id = 59)
 left join joomla.jml_eb_field_values v8 on (v8.registrant_id = r.id and v8.field_id = 38)
-where ASCII(UPPER(LEFT(CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,''),COALESCE(v8.field_value,'')),1))) NOT BETWEEN 64 AND 90 
-AND r.event_id = $EVENTID and ((v5.field_value != 'Speler' AND v7.field_value != 'No') or (v5.field_value = 'Keuken Crew' or v5.field_value = 'Spelleider')) 
+left join joomla.jml_eb_field_values medicsleep on (medicsleep.registrant_id = r.id and medicsleep.field_id = 71)
+where coalesce(substring_index(medicsleep.field_value,',',1),'') != 'Bastion' AND ASCII(UPPER(LEFT(CONCAT(COALESCE(v4.field_value,''),COALESCE(v3.field_value,''),COALESCE(v8.field_value,'')),1))) NOT BETWEEN 64 AND 90 
+AND r.event_id = $EVENTID and ((v5.field_value = 'Figurant' AND v7.field_value = 'Yes') or (v5.field_value = 'Keuken Crew' or v5.field_value = 'Spelleider')) 
 and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR 
 (r.published in (0,1) AND r.payment_method = 'os_offline'))
 UNION
@@ -63,8 +66,9 @@ v4.field_value as tweede_room from joomla.jml_eb_registrants r
 join joomla.jml_eb_field_values v1 on (v1.registrant_id = r.id and v1.field_id = 21)
 left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
 left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
+left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
 left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 71)
-where LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) = 'Bastion' AND r.event_id = $EVENTID 
+where v5.field_value = 'Speler' AND LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) = 'Bastion' AND r.event_id = $EVENTID 
 and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
 (r.published in (0,1) AND r.payment_method = 'os_offline'))
 UNION
@@ -72,12 +76,13 @@ UNION
 This TSQL Statement grabs data for medical sleepers in the tweede gebouw
 */
 select r.id, v1.field_value as name, LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) as building, v3.field_value as bastion_room,
-substring_index(LEFT(v6.field_value,LOCATE(' - ',v6.field_value) - 1),',',-1) as tweede_room from joomla.jml_eb_registrants r
+substring_index(v6.field_value,',',-1) as tweede_room from joomla.jml_eb_registrants r
 join joomla.jml_eb_field_values v1 on (v1.registrant_id = r.id and v1.field_id = 21)
 left join joomla.jml_eb_field_values v3 on (v3.registrant_id = r.id and v3.field_id = 37)
 left join joomla.jml_eb_field_values v4 on (v4.registrant_id = r.id and v4.field_id = 38)
+left join joomla.jml_eb_field_values v5 on (v5.registrant_id = r.id and v5.field_id = 14)
 left join joomla.jml_eb_field_values v6 on (v6.registrant_id = r.id and v6.field_id = 71)
-where LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) = 'tweede gebouw' AND r.event_id = $EVENTID
+where v5.field_value = 'Speler' AND LEFT(v6.field_value,LOCATE(',',v6.field_value) - 1) = 'tweede gebouw' AND r.event_id = $EVENTID
 and ((r.published = 1 AND (r.payment_method = 'os_ideal' OR r.payment_method = 'os_paypal' OR r.payment_method = 'os_bancontact')) OR
 (r.published in (0,1) AND r.payment_method = 'os_offline'));
 ");
